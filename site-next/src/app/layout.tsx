09e3afe8a39eb7b944bad20b26b1d318d6f5992b
illmedicine <demarkuswilsone@gmail.com>
2026-05-08 09:23:30 -0400

    Add Google Analytics gtag.js (G-DG1SB2ZNCW) to all pages

--- site-next/src/app/layout.tsx
+++ site-next/src/app/layout.tsx
@@ -28,15 +28,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager + Google tag (gtag.js G-DG1SB2ZNCW) */}
         <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5MFRRBFP');`
+          })(window,document,'script','dataLayer','GTM-5MFRRBFP');
+          (function(d){var g=d.createElement('script');g.async=true;
+          g.src='https://www.googletagmanager.com/gtag/js?id=G-DG1SB2ZNCW';
+          d.head.appendChild(g);})(document);
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', 'G-DG1SB2ZNCW');`
         }} />
-        {/* End Google Tag Manager */}
+        {/* End Google Tag Manager + gtag.js */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {/* Google Tag Manager (noscript) */}
